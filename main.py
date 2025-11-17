7412dcba97fc58723d7664a3a7789f9972b790c6
Anuragpathak08 <anuragasp123@gmail.com>
2025-11-17 23:28:16 +0530

    day 2 adding html page to the webserver

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import socket
-
 HOST = "127.0.0.1"
 PORT = 9000
 
-RESPONSE = b"""\
-HTTP/1.1 200 OK
-Content-type: text/html
-Content-length: 15
+with open("web-server/index.html", "rb") as f:
+    body = f.read()
 
-<h1>Hello!</h1>""".replace(b"\n", b"\r\n")
+content_length = len(body)
 
-with socket.socket() as server_sock:
-    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_sock.bind((HOST, PORT))
-    server_sock.listen(0)
-    print(f"Listening on {HOST}:{PORT}...")
+response = (
+    f"HTTP/1.1 200 OK\r\n"
+    f"Content-Type: text/html\r\n"
+    f"Content-Length: {content_length}\r\n"
+    f"\r\n"
+).encode("utf-8") + body
+
+with socket.socket() as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen()
+    print("Listening on", PORT)
 
     while True:
-        client_sock, client_addr = server_sock.accept()
-        print(f"New connection from {client_addr}.")
-        with client_sock:
-            client_sock.sendall(RESPONSE)
+        conn, addr = s.accept()
+        with conn:
+            request = conn.recv(2048)
+            print("REQUEST:\n", request.decode(errors="ignore"))
+
+            conn.sendall(response)
