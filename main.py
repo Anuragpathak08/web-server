78e3a074d021b92f6e35dbab7d85a465a28d790a
Anuragpathak08 <anuragasp123@gmail.com>
2025-11-21 23:29:10 +0530

    implemented handling css file

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import socket
+import re
+
 HOST = "127.0.0.1"
 PORT = 9000
 
-with open("web-server/index.html", "rb") as f:
-    body = f.read()
 
-content_length = len(body)
-
-response = (
-    f"HTTP/1.1 200 OK\r\n"
-    f"Content-Type: text/html\r\n"
-    f"Content-Length: {content_length}\r\n"
-    f"\r\n"
-).encode("utf-8") + body
+def response(content_type,path):
+    with open(path, "rb") as f:
+        body = f.read()
+    content_length = len(body)
+    response = (
+            f"HTTP/1.1 200 OK\r\n"
+            f"Content-Type: text/{content_type}\r\n"
+            f"Content-Length: {content_length}\r\n"
+            f"\r\n"
+            ).encode("utf-8") + body
+    return response
 
 with socket.socket() as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -24,6 +27,10 @@ with socket.socket() as s:
         conn, addr = s.accept()
         with conn:
             request = conn.recv(2048)
-            print("REQUEST:\n", request.decode(errors="ignore"))
-
-            conn.sendall(response)
+            r = request.decode(errors="ignore").split("\n")[0]
+            search = re.search(r"/\S+\.css",r )
+            if search:
+                conn.sendall(response("css",search.group(0)[1::]))
+            else:
+                conn.sendall(response("html", "index.html"))
+            print("REQUEST:\n", s , request)
